8b85e70a89e700b34f6fd1eb8968585b2e5b9d72
95rolancia <95jjangjun@gmail.com>
2020-12-03 15:50:37 +0900

    JS: remove active from nav

--- main.js
+++ main.js
@@ -25,12 +25,58 @@ homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
-// Make home transparent slowly
+// Make home slowly fade to transparent as the window scrolls down
 const homeContainer = document.querySelector(".home__container");
 const homeContainerHeight = homeContainer.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   homeContainer.style.opacity =
     (homeContainerHeight - window.scrollY) / homeContainerHeight;
+});
+
+// Show arrow up button when scrolls down
+const arrowUpBtn = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY >= homeContainerHeight) {
+    arrowUpBtn.classList.add("visible");
+  } else {
+    arrowUpBtn.classList.remove("visible");
+  }
+});
+
+arrowUpBtn.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
+
+// Handle projects button
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+workBtnContainer.addEventListener("click", (event) => {
+  const filter =
+    event.target.dataset.filter || event.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  // Remove selection from the previous item and select the
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target =
+    event.target.nodeName === "BUTTON" ? event.target : event.target.parentNode;
+  target.classList.add("selected");
+
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
 });
 
 function scrollIntoView(selector) {
